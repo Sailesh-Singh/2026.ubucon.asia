6ef942c0f6572cc4ddd8540882e542bff5a19b9b
Sailesh Singh <sailesh36687@gmail.com>
2026-08-05 12:13:33 +0545

    Add Local Members and details

--- src/data/committeeMembers.ts
+++ src/data/committeeMembers.ts
@@ -1,3 +1,6 @@
+import buorenImage from "../assets/localteam/Buo-ren.jpg";
+import TonyYipImage from "../assets/localteam/tony.png";
+
 export const committeeMembers = [
   {
     name: "Youngbin Han",
@@ -20,13 +23,13 @@ export const committeeMembers = [
   {
     name: "Aryan Kaushik",
     team: "Global team & Ubuntu Member",
-    role: "Content, and Travel support",
+    role: "Content",
     profileImageUrl: "https://avatars.githubusercontent.com/u/34372791?v=4",
   },
   {
     name: "Sailesh Singh",
     team: "Global team  & Ubuntu Nepal, Ubuntu Member",
-    role: "Web & Documentation, Content",
+    role: "Web & Documentation, Content and Travel Support",
     profileImageUrl: "https://avatars.githubusercontent.com/u/58818777?v=4",
   },
   {
@@ -35,10 +38,35 @@ export const committeeMembers = [
     role: "Sponsorship, Marketing & Design",
     profileImageUrl: "https://avatars.githubusercontent.com/u/142494719?v=4",
   },
+  // Local Team
   {
     name: "RJ Hsiao",
-    team: "Local team & Ubuntu Taiwan",
+    team: "Local team & Ubuntu Taiwan & MozTW & Hacking Thursday x TOSSUG",
     role: "Travel Support, Content, Web",
     profileImageUrl: "https://avatars.githubusercontent.com/u/236116?v=4",
   },
+  {
+  name: "Shih-Yuan Lee (aka fourdollars)",
+  team: "Local team & Ubuntu Taiwan & Debian & Hacking Thursday x TOSSUG",
+  role: "Content",
+  profileImageUrl: "https://avatars.githubusercontent.com/u/39708",
+},
+{
+  name: "Buo-ren Lin",
+  team: "Local team & Ubuntu Taiwan & Hacking Thursday x TOSSUG",
+  role: "Content",
+  profileImageUrl: buorenImage.src,
+},
+{
+  name: "Shawn Wang",
+  team: "Local team & Ubuntu Taiwan & Kaohsiung Linux User Group (KaLUG)",
+  role: "Content",
+  profileImageUrl: "https://avatars.githubusercontent.com/u/346761",
+},
+{
+  name: "Tony Yip",
+  team: "Local team & Hong Kong Linux User Group (HKLUG)",
+  role: "Content, Travel Support",
+  profileImageUrl: TonyYipImage.src,
+},
 ];
